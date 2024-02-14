6452cde86a466f43ae2712d92c3876e3cf1fc0ec
Ingrid Freitas <guibisconsin@gmail.com>
2024-02-14 18:36:52 -0400

    commit initial

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#claro').click(function(){
+        $('#span1').slideDown()
+    })
+
+    $('#sim').click(function (){
+        $('#span2').slideDown()
+    })
+
+    $('#no').click(function(){
+        $('span').slideUp()
+    })
+})
